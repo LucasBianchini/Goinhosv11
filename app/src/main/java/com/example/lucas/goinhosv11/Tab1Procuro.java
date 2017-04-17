7c004b15d1e9c91ba91cbefab1a7e2d2bfba3bd6
LucasBianchini <lucas_bianchini1@hotmail.com>
2017-04-17 16:49:41 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lucas/goinhosv11/Tab1Procuro.java
@@ -0,0 +1,21 @@
+package com.example.lucas.goinhosv11;
+
+/**
+ * Created by Lucas on 17/04/2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1Procuro extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1procuro, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/lucas/goinhosv11/Tab3Dicas.java
@@ -0,0 +1,22 @@
+package com.example.lucas.goinhosv11;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Lucas on 17/04/2017.
+ */
+
+public class Tab3Dicas extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3dicas, container, false);
+
+        return rootView;
+    }
+}
